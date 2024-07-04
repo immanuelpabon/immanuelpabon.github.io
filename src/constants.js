4b572f441a31012709044157172df18cf6fef3b1
immanuelpabon <immanuel@chrysalix.org>
2024-07-04 10:26:48 -0400

    dialogue

--- src/constants.js
+++ src/constants.js
@@ -22,52 +22,69 @@ export const dialogueData = {
   "...",
   "I don't know how to talk to people.",
   "Don't tell anyone I told you this, but there's a hidden area on the path to the right, just go between the groups of yellow flowers.",
-  "I am going to pretend this conversation never happened."
+  "I am going to pretend this conversation never happened.",
   ],
   kid: [
-    `[large]AHA![/large]`,
-    `Thought you could sneak past me?`
+    `AHA!`,
+    `Thought you could just walk past the tape, like the rules don't apply to you? I THINK NOT!`,
+    `I am spreading my arms super wide, there's no way you're getting past me.`,
+    `Now go explore the rest of the park, no water-slide-roller-coaster-hybrid for you.`,
+    `begone`,
+    `...`,
+    `...`,
   ],
   info: [
     `Hi! Welcome to the information booth. This park was made with the Kaboom.js library which is a library for making games in JavaScript.
-    Text is rendered with HTML/CSS. Here's the github link to the code for the website <a href="https://github.com/immanuelpabon/immanuelpabon.github.io" target="_blank">here</a>.`,
+    Text is rendered with HTML/CSS. Here's the github link to the code for the <a href="https://github.com/immanuelpabon/immanuelpabon.github.io" target="_blank">website</a>.`,
+    `Sorry for the mess, I had to run around in a panic setting up after I was fired from my last job. All this mess just so he could give MY job to his stupid son.`,
+    `Not like I was BUILT for working with paintings or anything.`,
+    `I'm not bitter at all.`,
+    `...`,
+  ],
+  art: [
+    `Welcome to the art gallery. This will be where you can see all of Immanuel's art assets. Still under construction though, sorry dude.`,
+    `My dad owns this whole park, he was able to get me this job after I dropped out of college.`,
+    `I'm not even a fan of paintings, I am more into sculptures. I even did a job for that Sign-o dude last month.`,
+    `It's crazy, some of my best work. I went a little overboard though, made it out of solid gold.`,
+    `That's why I had to get this job, to buy more gold. I wonder where that sign dude ended up putting it...`,
+    `...`,
   ],
   june: [
-    `Zzz... (She's dreaming about finding bugs.)`
+    `Zzz... (She's dreaming about finding bugs.)`,
   ],
   down_sign: [
-    `Portfolio Park exit.`
+    `Portfolio Park exit (↓).`,
   ],
   up_sign: [
-    `Test`
+    `Water-slide-roller-coaster-hybrid (↑). (Free entry!)`,
   ],
   left_sign: [
-    `Test`
+    `Information Booth (←) Lake Path (↓). The sign is covered in blue paint.`,
   ],
   right_sign: [
-    `Test`
+    `Art Gallery (↑) Show Stage (→).`,
   ],
   entrance_sign: [
-    `Portfolio Park. Parking: $12.00.`
+    `Portfolio Park (↑). Parking: $12.00.`,
   ],
   lake_sign: [
-    "Almost-A-Square Lake."
+    "Almost-A-Square Lake.",
   ],
   smile_tree: [
-    "I'm a tree!"
+    "I'm a tree!",
   ],
   koner: [
-    "Howdy, name's Keoni Smith. I'm Immanuel's cousin. I made the music you're listening to right now if you have audio enabled."
+    "Howdy, name's Keoni Smith. I'm Immanuel's cousin. I made the music you're listening to right now if you have audio enabled.",
+    "♫ Hmmmmmmmmm hmmmmm hmmmmmm... ♫",
   ],
   statue: [
-    "Derek the Sign. Artist Commision. Do not steal."
+    "Derek the Sign. Artist Commision. Do not steal.",
   ],
   resume: [
-    `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+    `It's a resume. Its covered in blue paint.`,
   ],
   exit: [
-    `If you want to exit Immanuel's portfolio, You can close the tab. The world drops off here.`
+    `If you want to exit Immanuel's portfolio, You can close the tab. The world drops off here.`,
   ],
 };
 /* tv: `That's my TV. I've been watching tech youtubers a lot recently like :
